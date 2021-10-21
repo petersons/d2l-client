b55552781e30eaa30cd9d345098dfa67f07a1775
Antonio Pauletich <antonio.pauletich95@gmail.com>
2021-10-21 16:22:36 +0200

    Add tests for the service provider

---
+++ tests/Unit/Laravel/ServiceProviderTest.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Laravel;
+
+use Illuminate\Container\Container;
+use Illuminate\Contracts\Config\Repository;
+use Illuminate\Contracts\Foundation\Application;
+use Petersons\D2L\AuthenticatedUriFactory;
+use Petersons\D2L\Contracts\ClientInterface;
+use Petersons\D2L\Laravel\ServiceProvider;
+use PHPUnit\Framework\TestCase;
+
+final class ServiceProviderTest extends TestCase
+{
+    public function testItRegistersTheHttpClient(): void
+    {
+        $container = new Container();
+        $repository = $this->createMock(Repository::class);
+        $repository->method('get')->with('d2l')->willReturn([
+            'host' => 'https://petersonstest.brightspace.com',
+            'app_id' => 'baz',
+            'app_key' => 'qux',
+            'lms_user_id' => 'foo',
+            'lms_user_key' => 'bar',
+            'org_id' => 'quux',
+            'installation_code' => 'quuz',
+            'p_key' => 'corge',
+            'api_lp_version' => '1.30',
+            'api_le_version' => '1.53',
+        ]);
+
+        $container->instance(Repository::class, $repository);
+
+        $serviceProvider = new ServiceProvider($container);
+        $serviceProvider->register();
+
+        $httpClient = $container->make(ClientInterface::class);
+        $this->assertInstanceOf(ClientInterface::class, $httpClient);
+    }
+
+    public function testItRegistersTheAuthenticatedUriFactory(): void
+    {
+        $container = new Container();
+        $repository = $this->createMock(Repository::class);
+        $repository->method('get')->with('d2l')->willReturn([
+            'host' => 'https://petersonstest.brightspace.com',
+            'app_id' => 'baz',
+            'app_key' => 'qux',
+            'lms_user_id' => 'foo',
+            'lms_user_key' => 'bar',
+            'org_id' => 'quux',
+            'installation_code' => 'quuz',
+            'p_key' => 'corge',
+            'api_lp_version' => '1.30',
+            'api_le_version' => '1.53',
+        ]);
+
+        $container->instance(Repository::class, $repository);
+
+        $serviceProvider = new ServiceProvider($container);
+        $serviceProvider->register();
+
+        $authenticatedUriFactory = $container->make(AuthenticatedUriFactory::class);
+        $this->assertInstanceOf(AuthenticatedUriFactory::class, $authenticatedUriFactory);
+    }
+
+    public function testItPublishesTheConfig(): void
+    {
+        $app = $this->createMock(Application::class);
+        $app->expects($this->once())->method('runningInConsole')->willReturn(true);
+        $app->expects($this->once())->method('configPath')->with('d2l.php')->willReturn($path = '/foo/d2l.php');
+
+        $serviceProvider = new ServiceProvider($app);
+        $serviceProvider->boot();
+
+        $this->assertSame([
+            ServiceProvider::class => [
+                realpath(__DIR__.'/../../../src/Laravel/config/d2l.php') => $path
+            ]
+        ], ServiceProvider::$publishes);
+
+        $this->assertSame([
+            'd2l-config' => [
+                realpath(__DIR__.'/../../../src/Laravel/config/d2l.php') => $path
+            ]
+        ], ServiceProvider::$publishGroups);
+    }
+}
